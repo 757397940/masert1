a17aba53fac4a9158561244f6a5644748ec72a73
Administrator <757397940@qq.com>
2019-01-10 15:42:39 +0800

    提交项目

--- application/common/model/MemberVerify.php
+++ application/common/model/MemberVerify.php
@@ -8,4 +8,21 @@ class MemberVerify extends Common{
      * @param int $type 1注册，2找回密码，3更换绑定 4验证登录
      * @param int $uid
      */
+    public function addData($mobile='',$type = 1, $uid = 0){
+        if(!$mobile){
+            $this->error = "手机号不能为空";
+            return false;
+        }
+        $where = array();
+        $where['mobile'] = $mobile;
+        $where['status'] = 0;
+        $where['type'] = $type;
+        if($uid){
+            $where['uid'] = $uid;
+        }
+        $data = array();
+        $info = $this->where($where)->find();
+
+
+    }
 }
